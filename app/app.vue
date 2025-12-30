abc0f14295e4e4a56e6ee209e10adb166321f9a2
Edwin Samodra <edwinsp001@gmail.com>
2025-12-30 15:54:28 +0700

    adjust useHead, useSeoMeta, header, footer

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,12 @@
-<script setup>
+<script setup lang="ts">
+const colorMode = useColorMode()
+colorMode.preference = 'dark'
+
 useHead({
   meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { key: 'theme-color', name: 'theme-color', content: "#1b1718" }
   ],
   link: [
     { rel: 'icon', href: '/favicon.ico' }
@@ -11,34 +16,75 @@ useHead({
   }
 })
 
-const title = 'Nuxt Starter Template'
-const description = 'A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.'
+const title = 'Edwin Samodra Pratama | Personal Website'
+const description = 'Personal website of Edwin Samodra Pratama — full-stack developer'
+const image = 'https://media.licdn.com/dms/image/v2/D5603AQGSERAYIGoZmg/profile-displayphoto-shrink_400_400/B56ZTmAY2EHQAg-/0/1739025650542?e=1768435200&v=beta&t=jL-x8E5B9CdoiD31p1EFCMmNc-gagFwXVPKPGAtvkm8'
+const url = 'https://edwinsamodra.com'
 
 useSeoMeta({
+  /* ---------- Basic SEO ---------- */
   title,
   description,
+  robots: 'index, follow',
+  charset: 'utf-8',
+  viewport: 'width=device-width, initial-scale=1',
+
+  /* ---------- Open Graph ---------- */
   ogTitle: title,
   ogDescription: description,
-  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
-  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
-  twitterCard: 'summary_large_image'
+  ogSiteName: 'Edwin Samodra Pratama',
+  ogUrl: url,
+  ogType: 'website',
+  ogLocale: 'en_US',
+  ogImage: image,
+  ogImageAlt: 'Edwin Samodra Pratama profile photo',
+
+  /* ---------- Twitter ---------- */
+  twitterCard: 'summary_large_image',
+  twitterTitle: title,
+  twitterDescription: description,
+  twitterImage: image,
+  twitterCreator: '@edwinsamodra', // hapus kalau belum punya
+  twitterSite: '@edwinsamodra'     // hapus kalau belum punya
 })
+
+
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const items = computed<NavigationMenuItem[]>(() => [{
+  label: 'Home',
+  to: '/#',
+  active: false
+  }, {
+  label: 'Experience',
+  to: '/#experience',
+  active: false
+}, {
+  label: 'Projects',
+  to: '/#projects',
+  active: false
+}, {
+  label: 'About',
+  to: '/#about',
+  active: false
+}, {
+  label: 'Contact Me',
+  to: 'mailto:hi@edwinsamodra.com',
+}])
 </script>
 
 <template>
   <UApp>
     <UHeader>
-      <template #left>
+      <template #title>
         <NuxtLink to="/">
-          <AppLogo class="w-auto h-6 shrink-0" />
+          <p class="font-extrabold text-4xl font-sans tracking-tighter subpixel-antialiased">ED</p>
         </NuxtLink>
-
-        <TemplateMenu />
       </template>
 
-      <template #right>
-        <UColorModeButton />
+      <UNavigationMenu :items="items" />
 
+      <template #right>
         <UButton
           to="https://github.com/nuxt-ui-templates/starter"
           target="_blank"
@@ -46,6 +92,26 @@ useSeoMeta({
           aria-label="GitHub"
           color="neutral"
           variant="ghost"
+        />
+
+        <UButton
+          to="https://www.linkedin.com/in/edwinsamodra/"
+          target="_blank"
+          icon="i-simple-icons-linkedin"
+          aria-label="LinkedIn"
+          color="neutral"
+          variant="ghost"
+        />
+      </template>
+
+      <template #body>
+        <UNavigationMenu
+          :items="items"
+          orientation="vertical"
+          class="w-full"
+          :ui="{
+            item: 'p-1 mb-2 bg-zinc-800 rounded font-medium'
+          }"
         />
       </template>
     </UHeader>
@@ -54,24 +120,13 @@ useSeoMeta({
       <NuxtPage />
     </UMain>
 
-    <USeparator icon="i-simple-icons-nuxtdotjs" />
+    <USeparator />
 
     <UFooter>
-      <template #left>
-        <p class="text-sm text-muted">
-          Built with Nuxt UI • © {{ new Date().getFullYear() }}
+      <template #default>
+        <p class="text-sm text-muted my-2">
+          © {{ new Date().getFullYear() }} · Edwin Samodra Pratama
         </p>
-      </template>
-
-      <template #right>
-        <UButton
-          to="https://github.com/nuxt-ui-templates/starter"
-          target="_blank"
-          icon="i-simple-icons-github"
-          aria-label="GitHub"
-          color="neutral"
-          variant="ghost"
-        />
       </template>
     </UFooter>
   </UApp>
