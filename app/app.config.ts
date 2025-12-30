33203ba7f1a8cd0f8e471be0c46d208cdfc4dfaf
Edwin Samodra <edwinsp001@gmail.com>
2025-12-30 15:53:36 +0700

    define theme and max width

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,18 @@
 export default defineAppConfig({
   ui: {
     colors: {
-      primary: 'green',
-      neutral: 'slate'
+      primary: 'zinc',
+      neutral: 'zinc'
+    },
+
+    container: {
+      base: `
+        max-w-4xl
+        px-3
+        sm:px-4
+        lg:px-5
+        xl:px-6
+      `
     }
   }
 })
